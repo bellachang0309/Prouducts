7690d96b4db2b1d65f03baea3e79d30a9d481436
Bella Chang <91xa14@ms.mingdao.edu.tw>
2021-08-11 21:44:32 +0800

    add read file

--- prouducts.py
+++ prouducts.py
@@ -1,4 +1,15 @@
+
+#讀取檔案
 prouducts=[]
+with open('prouducts.csv','r')as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name,price=line.strip().split(',')
+		prouducts.append([name,price])
+print(prouducts)
+
+#讓使用者輸入
 while True:
 	name=input('請輸入商品名稱：')
 	if name=='q':
@@ -7,9 +18,11 @@ while True:
 	prouducts.append([name,price])
 print(prouducts)
 
+#印出所有購買紀錄
 for a in prouducts:
 	print(a[0],'的價格是',a[1])
 
+#寫入檔案
 with open('prouducts.csv','w')as f:
 	f.write('商品,價格\n')
 	for p in prouducts:
